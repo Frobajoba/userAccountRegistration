44dfb16292dd96a3eeba04c19ffd8d4aa779ce44
James_Cox <Jecox19@gmail.com>
2019-08-03 22:23:56 +0100

    Changed method names slightly, less confusing

--- UserInputChecker.cs
+++ UserInputChecker.cs
@@ -3,18 +3,18 @@ namespace UserRegistrationApp
 {
   class UserInputChecker
   {
-    public static void firstName(String firstName)
+    public static void firstName(String first)
     {
-      if (string.IsNullOrWhiteSpace(firstName))
+      if (string.IsNullOrWhiteSpace(first))
       {
         StandardMessages.userInputError("first");
         StandardMessages.endApplication();
       }
     }
 
-    public static void lastName(String lastName)
+    public static void lastName(String last)
     {
-      if (string.IsNullOrWhiteSpace(lastName))
+      if (string.IsNullOrWhiteSpace(last))
       {
         StandardMessages.userInputError("last");
         StandardMessages.endApplication();
